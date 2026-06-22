ee308430d3f649c875633df35b1c44232a57e364
Michael <michan.pugachev@yandex.ru>
2026-06-22 19:48:22 +0300

    hw-4

--- js/main.js
+++ js/main.js
@@ -1,106 +1,17 @@
 'use strict'
 
-// import * as func from './modules/mobile.js';
-//import * as str from './modules/string-methods.js';
-//import * as math from './modules/math-methods.js';
-//import * as date from './modules/date-methods.js';
-//import * as regExp from './modules/RegExp-methods.js';
+
 import * as func from './modules/homework.js';
-import * as call from './modules/callback.js';
-import * as settime from './modules/timeout.js';
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
       
-    //  str.fn_task_LowerCase(); 
-    //  str.fn_task_indexOf();
-    //  str.fn_task_includes();
-   //   str.fn_task_startsWith();
-   //   console.log(str.palindrom('янестарбратсеня'));
-
-   //     math.fn_task_Random(20,30);
-
-    //     date.fn_task_CurrentDate();
-    //    date.fn_task_LastDate(1);
-    //    date.fn_task_Desired_format();
-    //      date.fn_task_Formating();
-    //      date.fn_task_Current_Time_Formating();
-
-    //    regExp.fn_task_name_verification();
-    //    regExp.fn_task_selection();
-
-    // Встроенные объекты
-    // func.fn_task_1();  
-     
-    //func.fn_task_2();
-        
-    //  func.fn_task_3(30);
-    //    func.fn_task_4();
-   //    func.fn_task_5(func.sayHi,'Глеб','Петров');
-     //  
-    //  func.fn_task_6(17);
-     //   func.fn_task_7(33,12);  
-     // func.fn_task_8();  
-     // func.fn_task_9();
-    //  func.fn_task_10();  
-
-      //func.fn_task_11();
-      //func.fn_task_12();
-      //func.fn_task_13();
-      //func.fn_task_14();
-     // func.fn_task_15();   
-
-    //  function first(callback){
-    //     console.log('Привет');
-    //     callback();
-    //   }
-    //   function second(){
-    //       console.log(" мир");
-    //   }
-    //   first(second);
-
-      
-     /* 
-      call.callbackWithArrayLength([1], (length) => {
-	        console.log(length);
-     });
-
-    call.callbackWithArrayLength([1, 1], (len) => {
-	        console.log(len);
-    });
-    call.callbackWithArrayLength([1, 1, 1, 1, 1], (l) => {
-	        console.log(l);
-    });
-   */
-  /*
-    call.each(["Глеб", "Олег", "Татьяна", "Платон"],call.log);
-    
-    console.log(call.map([1, 4, 9, 16], call.square)); 
-    console.log(call.map([1, 4, 9, 16], call.squareRoot));
-  */
-/*
-    console.log(call.filter([3, -4, 1, 9], call.isPositive)); 
-
-    const people = [
-        {name: 'Глеб', gender: 'male'},
-        {name: 'Анна', gender: 'female'},
-        {name: 'Олег', gender: 'male'},
-        {name: 'Оксана', gender: 'female'}
-    ];
-    
-    console.log(call.filter(people, call.isMale));
-*/
-//settime.liveCountdown(9);
-//const deadline = prompt('На сколько секунд вы хотите поставить таймер?');
-//settime.countdown(deadline);
-
-
-
-// Функция sayHi выводит в консоль приветствие для указанного имени
-
-
-// Код выше менять нельзя
-
-// Нужно изменить код ниже:
-
-
+     func.fn_task_1();
+     func.fn_task_2();
+     func.fn_task_3();
+     func.fn_task_4();
+     func.fn_task_5();
+     func.fn_task_6();
+     func.fn_task_7();
 });
