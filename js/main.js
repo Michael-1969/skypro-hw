c29427cd4acd5e7b4b41c1d40ba3f2d4b52455b7
Michael <michan.pugachev@yandex.ru>
2026-06-22 17:19:03 +0300

    hw-2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 'use strict'
 
-import * as func from './modules/mobile.js';
-
+// import * as func from './modules/mobile.js';
+import * as str from './modules/string-methods.js';
+import * as math from './modules/math-methods.js';
+import * as date from './modules/date-methods.js';
+import * as regExp from './modules/RegExp-methods.js';
+import * as func from './modules/homework.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-      console.log("Z");
       
-      func.fn_greeting();
+    //  str.fn_task_LowerCase(); 
+    //  str.fn_task_indexOf();
+    //  str.fn_task_includes();
+   //   str.fn_task_startsWith();
+   //   console.log(str.palindrom('янестарбратсеня'));
+
+   //     math.fn_task_Random(20,30);
+
+    //     date.fn_task_CurrentDate();
+    //    date.fn_task_LastDate(1);
+    //    date.fn_task_Desired_format();
+    //      date.fn_task_Formating();
+    //      date.fn_task_Current_Time_Formating();
+
+    //    regExp.fn_task_name_verification();
+    //    regExp.fn_task_selection();
+
+    // Встроенные объекты
+    //  func.fn_task_1();  
+     //task 2
+    //    const arr_strings = ['Крепкий малыш','Капитан Врунгель','Один дома','Малыш и Карлсон','Приключение Буратино','Один в поле не воин'];
+    //    const single_string = 'малыш';
+    //    func.fn_task_2(arr_strings,single_string);
+
+
+      // func.fn_task_3();
+     //  func.fn_task_4();
+     //  func.fn_task_5();
+    //  func.fn_task_6(17);
+     //   func.fn_task_7(33,12);  
+     // func.fn_task_8();  
+     // func.fn_task_9();
+      func.fn_task_10();  
+
+      //func.fn_task_11();
+      //func.fn_task_12();
+      //func.fn_task_13();
+      //func.fn_task_14();
+     // func.fn_task_15();   
   
 });
